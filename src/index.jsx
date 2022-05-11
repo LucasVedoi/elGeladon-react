7e547433535ae32454bd42d3fad360b2b3ca95de
LucasVedoi <89050672+LucasVedoi@users.noreply.github.com>
2022-05-10 21:31:12 -0300

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./views/Home/Home";
+import App from "App";
+import "assets/styles/main.css";
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+  <App />
+</React.StrictMode>,
+
+  document.getElementById("root")
+);
